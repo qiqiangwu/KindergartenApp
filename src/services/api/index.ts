581982c726e8c54805a0a656f260c3cdccee848c
qiqiangwu <qiqiangwu@live.cn>
2022-03-10 15:18:20 +0800

    aumsApi对象添加到ApiService

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,12 +1,15 @@
-import {CounterApi} from './counter';
+import { AumsApi } from './aums';
+import { CounterApi } from './counter';
 
 export class ApiService implements IService {
   private inited = false;
 
   counter: CounterApi;
+  aums: AumsApi;
 
   constructor() {
     this.counter = new CounterApi();
+    this.aums = new AumsApi('http://219.233.221.231:38080');
   }
 
   init = async (): PVoid => {
